3c6ecdeb20f6feff4a8be686f5f8ee95a562626f
muhammad burhan din <burhandin87@gmail.com>
2025-02-11 22:25:55 +0800

    initial mvc

---
+++ my-web-app-2025/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace my_web_app_2025.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
